94444ce02f4169e091ca6910b5e8790e567b0403
Sroka, Lukasz (LI - External) <lukasz.sroka@externals.hilti.com>
2025-07-13 15:40:53 +0200

    consttionString

--- Backend/Store/Store/Program.cs
+++ Backend/Store/Store/Program.cs
@@ -6,7 +6,14 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        var host = CreateHostBuilder(args).Build();
+
+        using (var scope = host.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.Migrate();
+        }
+        host.Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
